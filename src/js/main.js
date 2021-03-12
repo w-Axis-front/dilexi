a02c0cb88f92ac530d28844d15b1180e9db0e071
fedorova_kateryna <K_phedorova@ukr.net>
2021-03-12 18:01:05 +0200

    feat(main): add

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 // Main JS module
+import $ from "jquery";
 // objectFitImages polyfill
 import objectFitImages from "object-fit-images";
+import scrollContent from "./modules/scrollContent";
 
 $(function() {
   objectFitImages();
+  scrollContent();
 });
