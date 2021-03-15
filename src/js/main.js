d988df55d3d40898cc443455c2f972b3a6246132
fedorova_kateryna <K_phedorova@ukr.net>
2021-03-15 18:10:29 +0200

    feat(main): add navbar, add main adaptiveness, add timer and animate main button

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,13 @@
 import $ from "jquery";
 // objectFitImages polyfill
 import objectFitImages from "object-fit-images";
+import nav from "./modules/nav";
 import scrollContent from "./modules/scrollContent";
+import countTime from "./modules/countTime";
 
 $(function() {
   objectFitImages();
+  nav();
   scrollContent();
+  countTime();
 });
